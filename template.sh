1c4be95f28036299d4ed9c0bbaf4173f14cfc5ab
Muhammad Hamza Saeed Khan <hamza.khan6093@gmail.com>
2025-12-11 00:44:09 +0500

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# Creating a Folder 
+mkdir -p src
+mkdir -p research
+
+
+# Creating a Files 
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch app.py
+touch research/troals.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
